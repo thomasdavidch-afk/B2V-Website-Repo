57ba79fe40b7848006e50998fb8cdbc00623850d
thomasdavidch-afk <thomas.david.ch@gmail.com>
2026-09-01 11:02:24 +0200

    Add Legal Pages

--- frontend/Router/allRoutes.js
+++ frontend/Router/allRoutes.js
@@ -6,6 +6,9 @@ export const allRoutes = [
     new Route("/contact", "Contact", "/pages/contact.html", []),
     new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"]),
     new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"]),
+    new Route("/mentions-legales", "Mentions légales", "/pages/mentions-legales.html", []),
+    new Route("/reglement-interieur", "Règlement Intérieur", "/pages/reglement-interieur.html", []),
+    new Route("/cgv", "CGV", "/pages/cgv.html", []),
 ];
 
 // Le titre affiché sur l'onglet
