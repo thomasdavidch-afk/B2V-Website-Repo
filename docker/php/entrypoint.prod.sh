e3ae9d0306f1645a9c7e779333082475bd0dbdca
thomasdavidch-afk <thomas.david.ch@gmail.com>
2026-09-07 14:55:10 +0200

    Modif Entrypoint et Nginxprod

--- docker/php/entrypoint.prod.sh
+++ docker/php/entrypoint.prod.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -e
 
+# Se placer dans le bon dossier
+cd /var/www/backend
+
 # Migrations auto sur MySQL Aiven
-php bin/console doctrine:migrations:migrate --no-interaction || true
+php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration || true
 
 # Préchauffage du cache Symfony
 php bin/console cache:clear --no-interaction || true
